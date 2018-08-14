9bc22e64538ed8b20869966f1cef1ca312f62ffd
Dahyun <honeybeat1@gmail.com>
2018-08-14 17:20:52 +0900

    Class 추가

--- test_vm.py
+++ test_vm.py
@@ -1,21 +1,21 @@
-from vm import run, init  #모르는 함수 나오면 항상 임포트 해주새요 
+from vm import VendingMachine #run, init  #모르는 함수 나오면 항상 임포트 해주새요 
 
 
 def test_initial_change_should_be_zero():
-	init()
-	assert "잔액은 0원입니다" == run("잔액")
+	m = VendingMachine()
+	assert "잔액은 0원입니다" == m.run("잔액")
 
 def test_insert_coin_and_check():
-	init()
-	assert "100원을 넣었습니다" == run("동전 100")
-	assert "잔액은 100원입니다" == run("잔액")
+	m = VendingMachine()
+	assert "100원을 넣었습니다" == m.run("동전 100")
+	assert "잔액은 100원입니다" == m.run("잔액")
 
 def test_accumulation_of_change():
- 	init()
- 	run("동전 100")
- 	run("동전 100")
- 	assert "잔액은 200원입니다" == run("잔액")
+ 	m = VendingMachine()
+ 	m.run("동전 100")
+ 	m.run("동전 100")
+ 	assert "잔액은 200원입니다" == m.run("잔액")
 
-def test_meow():
-	init()
-	assert "알 수 없는 명령입니다." == run("고양이")
+# def test_meow():
+# 	init()
+# 	assert "알 수 없는 명령입니다." == run("고양이")
--- vm.py
+++ vm.py
@@ -1,21 +1,46 @@
-change = 0
+#change = 0
+#precommand = ""
+
+class VendingMachine:
+	def __init__(self): #init = 메소드
+		self._change = 0 #change = 변수
+	
+	def run(self,raw): #run은 메소드가 되었다 
+		tokens = raw.split()
+		cmd, params = tokens[0], tokens[1:]
+		if cmd == "잔액":
+			return "잔액은 " + str(self._change) + "원입니다"
+		elif cmd == "동전" : 
+			coin = params[0]
+			self._change += int(coin) #100원을 int(coin)으로 대체
+			return coin + "원을 넣었습니다"
+		else:
+			return "알 수 없는 명령입니다"
 
 
-def run(raw):
-	global change #임시 방편
 
-	tokens = raw.split()
-	cmd, params = tokens[0], tokens[1:]
 
-	if cmd == "잔액":
-		return "잔액은 " + str(change) + "원입니다"
-	elif cmd == "동전" : 
-		coin = params[0]
-		change += int(coin) #100원을 int(coin)으로 대체
-		return coin + "원을 넣었습니다"
-	else:
-		return "알 수 없는 명령입니다."
 
-def init():  #매 케이스마다 초기화 시켜주는 함수 만들기
-	global change
-	change = 0
+# def run(raw):
+# 	global change #임시 방편
+# 	global precommand
+
+# 	tokens = raw.split()
+# 	cmd, params = tokens[0], tokens[1:]
+
+# 	if cmd == "잔액":
+# 		if precommand == "고양이":
+# 			return "야옹"
+# 		else:
+# 			return "잔액은 " + str(change) + "원입니다"
+# 	elif cmd == "동전" : 
+# 		coin = params[0]
+# 		change += int(coin) #100원을 int(coin)으로 대체
+# 		return coin + "원을 넣었습니다"
+# 	else:
+# 		precommand = raw
+# 		return "고양이를 넣었습니다"
+
+# def init():  #매 케이스마다 초기화 시켜주는 함수 만들기
+# 	global change
+# 	change = 0
