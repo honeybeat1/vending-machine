ce57eb8ef48edf3c43ea57a2c01adddc3bf20822
Dahyun <honeybeat1@gmail.com>
2018-08-28 10:40:53 +0900

    테스트 케이스 한글화

--- test_vm.py
+++ test_vm.py
@@ -1,15 +1,15 @@
 from vm import VendingMachine 
 
-def test_initial_change_should_be_zero():
+def test_초기_잔액은_0원():
 	m = VendingMachine()
 	assert "잔액은 0원입니다" == m.run("잔액")
 
-def test_insert_coin_and_check():
+def test_동전_넣고_잔액_검사():
 	m = VendingMachine()
 	assert "100원을 넣었습니다" == m.run("동전 100")
 	assert "잔액은 100원입니다" == m.run("잔액")
 
-def test_accumulation_of_change():
+def test_잔액_누적():
  	m = VendingMachine()
  	m.run("동전 100")
  	m.run("동전 100")
@@ -37,13 +37,13 @@ def test_동전이_부족한_상황에서_음료_뽑기():
     assert "잔액이 부족합니다" == m.run("음료 커피")
     assert "잔액은 100원입니다" == m.run("잔액")
 
-def test_valid_coins():
+def test_유효한_동전():
 	m = VendingMachine()
 	valid_coins = ['10','50','100','500']
 	for coin in valid_coins:
 		assert coin + "원을 넣었습니다" == m.run('동전 '+ coin)
 
-def test_invalid_coins():
+def test_유효하지_않은_동전():
 	m = VendingMachine()
 	assert "모르는 동전입니다" == m.run("동전 999")
 
